84f8ce97511403ecc1708a212cc6949b297ca2b7
fatimaaaaaaa <44090391+fatimaaaaaaa@users.noreply.github.com>
2022-05-18 20:16:52 +0200

    poopy poo styling

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -19,12 +19,11 @@ const NavBar = ({
       style={{
         color: 'white',
         backgroundColor: '#3ca341',
-        height: '15vh',
         paddingLeft: 16,
         alignContent: 'bottom'
       }}
     >
-      <ToolbarText>
+      <ToolbarText className="toolbartext">
         <Typography variant="h4" align="left" sx={{ mt: '30px' }}>
           <Link id="link" to={user === 'LaboSem' ? '/labosem' : '/union'}>
             TéléCiden
@@ -54,38 +53,48 @@ const NavBar = ({
             </Link>
           )}
         </MenuContainer>
-        <Typography
-          align="right"
-          sx={{
-            mt: '45px',
-            mr: '20px',
-            right: '10px'
-          }}
-        >
-          <b>Signed in as</b>: {user} user
-        </Typography>
-        <Button
-          variant="contained"
-          size="small"
-          sx={{ top: '5px', right: '25px', position: 'absolute' }}
-        >
-          <Link id="link" to="/">
-            Sign out
-          </Link>
-        </Button>
+        <div>
+          <Typography
+            align="right"
+            sx={{
+              mt: '45px',
+              mr: '20px',
+              right: '10px'
+            }}
+          >
+            <b>Signed in as</b>: {user} user
+          </Typography>
+          <Button
+            variant="contained"
+            size="small"
+            sx={{ top: '5px', right: '25px', position: 'absolute' }}
+          >
+            <Link id="link" to="/">
+              Sign out
+            </Link>
+          </Button>
+        </div>
       </ToolbarText>
     </AppBar>
   );
 };
 
-const ToolbarText = styled.span`
+const ToolbarText = styled.div`
   width: 100%;
   height: 100%;
   display: inline-flex;
 
-  & > p {
+  & > * > p {
     position: absolute;
     font-size: 0.9em;
+  }
+
+  @media screen and (max-width: 615px) {
+    display: block;
+    & > h4 {
+      font-size: 1.1em;
+      text-align: center;
+    }
   }
 `;
 
@@ -102,6 +111,10 @@ const MenuContainer = styled.div`
     top: 40px;
     font-size: 0.9em;
   }
+
+  @media screen and (max-width: 615px) {
+   display: block;
+
 `;
 
 export default NavBar;
